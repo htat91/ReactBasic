35f9b331704b283fc444ff09722ca2c7fc13204c
an thien <htat912000@gmail.com>
2023-10-25 11:54:36 +0700

    update src

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello world 
+          Wibu number 1
         </p>
         <a
           className="App-link"
